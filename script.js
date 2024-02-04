109b257aad424acc500b6c8260caca976a4e0b44
Krzysztof <krzysztof.rogacki@gmail.com>
2024-02-04 19:45:24 +0100

    Finishing challenge

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+const accordion = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+Object.keys(accordion).forEach(key => {
+  accordion[key].addEventListener("click", function() {
+      const panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        this.style.backgroundImage = "url('./assets/images/icon-plus.svg')";
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        this.style.backgroundImage = "url('./assets/images/icon-minus.svg')";
+      } 
+    });
+});
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.maxHeight) {
+//       panel.style.maxHeight = null;
+//     } else {
+//       panel.style.maxHeight = panel.scrollHeight + "px";
+//     } 
+//   });
+// }
